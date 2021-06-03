036f88d9ae90dbadafd17f2bab56f133789875ec
parazyd <parazyd@dyne.org>
2021-06-03 12:25:53 +0200

    Use larger ledger size for solana-test-validator helper script.

--- cli/local_validator_tmux.sh
+++ cli/local_validator_tmux.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf test-ledger
-tmux new-session -d "solana-test-validator"
+tmux new-session -d "solana-test-validator --limit-ledger-size 999999"
 sleep 1
 tmux split-window -v "solana logs"
 tmux a
